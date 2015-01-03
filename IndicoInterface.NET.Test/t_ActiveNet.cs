e739a598a8f96a751fd818958922cae68a0bcb70
Gordon Watts <gordon@gordonandpaula.net>
2015-01-03 08:29:25 +0100

    Get at least one real network test up and running.

--- IndicoInterface.NET.Test/t_ActiveNet.cs
+++ IndicoInterface.NET.Test/t_ActiveNet.cs
@@ -2,6 +2,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Net;
 using System.IO;
+using System.Threading.Tasks;
 
 namespace IndicoInterface.NET.Test
 {
@@ -27,144 +28,30 @@ namespace IndicoInterface.NET.Test
                 res.Close();
             }
         }
-#if false
-        [TestMethod]
-        public void GetSimpleMeetingAgenda()
+
+        /// <summary>
+        /// Simple URL reader that will fetch a URL from the web.
+        /// </summary>
+        class WebGetter : IUrlFetcher
         {
-            string url = "http://indico.cern.ch/conferenceDisplay.py?confId=33078";
-            AgendaInfo a = new AgendaInfo(url);
-            var data = a.GetFullConferenceData();
-            Assert.IsTrue(data.ID == "33078", "Conference ID was not filled correctly");
-            Assert.IsTrue(data.category == "B-tagging", "Catagory was incorrect!");
-            Assert.IsTrue(data.title == "CSC Note 10: Dijet Calibration Working Group Meeting", "Title is not correct");
-            Assert.IsTrue(data.startDate == "2008-04-29T16:30:00", "Start date is not right");
-            Assert.IsTrue(data.endDate == "2008-04-29T19:25:00", "End date for meeting is not right");
-
-            Assert.IsTrue(data.session == null, "The conference should have no sessions.");
-            Assert.IsTrue(data.contribution.Length == 4, "Didn't find the expected 4 talks");
-            Assert.IsTrue(data.contribution[0].ID == "2", "ID of first contribution talk is incorrect");
-            Assert.IsTrue(data.contribution[0].startDate == "2008-04-29T16:30:00", "Start date of first contribution is not right");
-            Assert.IsTrue(data.contribution[0].endDate == "2008-04-29T16:45:00", "End date of contribution is not right");
-            Assert.IsTrue(data.contribution[0].speakers != null, "No speakers for talk!");
-            Assert.IsTrue(data.contribution[0].speakers.Length == 1, "Speakers shoudl have only one entry in here!");
-            Assert.IsTrue(data.contribution[0].speakers[0].users != null, "User should not be null!");
-            Assert.IsTrue(data.contribution[0].speakers[0].users.Length == 1, "There should be only one user!");
-            Assert.IsTrue(data.contribution[0].speakers[0].users[0].name != null, "User's name should be non-szero!");
-            Assert.IsTrue(data.contribution[0].speakers[0].users[0].name.first == "", "User's name is wrong!");
-            Assert.IsTrue(data.contribution[0].speakers[0].users[0].name.middle == "", "User's name is wrong!");
-            Assert.IsTrue(data.contribution[0].speakers[0].users[0].name.last == "Orin Harris (University of Washington)", "User's name is wrong!");
-
-            var firsttalk = data.contribution[0].material;
-            Assert.IsTrue(firsttalk != null, "The material for first talk is a null!");
-            Assert.IsTrue(firsttalk.Length == 1, "There should be only one type of material in the talk");
-            Assert.IsTrue(firsttalk[0].ID == "slides", "The material type should be slides!");
-            Assert.IsTrue(firsttalk[0].title == "Slides", "The material title should be slides!");
-            Assert.IsTrue(firsttalk[0].files.file.Length == 1, "There should be only one file associated with this talk");
-            Assert.IsTrue(firsttalk[0].files.file[0].name == "s8_29_04_08.pdf", "The talk file is named incorrectly");
-            Assert.IsTrue(firsttalk[0].files.file[0].type == "pdf", "The talk file type is not pdf");
-            Assert.IsTrue(firsttalk[0].files.file[0].url.StartsWith("http://indico.cern.ch"), "Url of talk doesn't look right");
-
-            Assert.IsTrue(data.contribution[1].material[0].link == null, "Link for second talk doesn't seem to be correct.");
+            public async Task<StreamReader> GetDataFromURL(Uri uri)
+            {
+                var req = WebRequest.Create(uri);
+                (req as HttpWebRequest).UserAgent = "DeepTalk AgentaInfo Test";
+                var response = await req.GetResponseAsync();
+                return new StreamReader(response.GetResponseStream());
+            }
         }
-#endif
 
-#if false
         [TestMethod]
-        public void GetConferenceMeetingAgenda()
+        public async Task GetFullMeetingInfo()
         {
-            AgendaInfo a = new AgendaInfo(14475);
-            var data = a.GetFullConferenceData();
+            var a = new AgendaInfo("https://indico.cern.ch/event/340656/");
+            var al = new AgendaLoader(new WebGetter());
+            var data = await al.GetNormalizedConferenceData(a);
 
-            Assert.IsTrue(data.ID == "14475", "Conference ID is incorrect.");
-            Assert.IsTrue(data.category == "B-tagging", "Conference catagory is incorrect.");
-            Assert.IsTrue(data.title == "ATLAS b-tagging workshop", "Conference title is incorrect");
-            Assert.IsTrue(data.contribution == null, "The conference should have no contributions!");
-            Assert.IsTrue(data.session != null, "The conference should have sessions.");
-            Assert.IsTrue(data.session.Length == 11, "The meeting does not have the right number of sessions!");
-
-            Assert.IsTrue(data.session[0].ID == "8", "The first session ID is not 8");
-            Assert.IsTrue(data.session[1].ID == "11", "The second session ID is not 11");
-
-            Assert.IsTrue(data.session[0].title == "Introduction I", "Session title is not right");
-            Assert.IsTrue(data.session[0].startDate == "2007-05-10T09:15:00", "session start date is not right");
-            Assert.IsTrue(data.session[0].endDate == "2007-05-10T10:30:00", "session start date is not right");
-
-            Assert.IsTrue(data.session[0].contribution.Length == 5, "First session has incorrect number of contributions");
-            Assert.IsTrue(data.session[1].contribution.Length == 4, "Second session has incorrect number of contributions");
-
-            Assert.IsTrue(data.session[0].contribution[0].ID == "21", "First contribution ID is not correct");
-            Assert.IsTrue(data.session[0].contribution[4].ID == "23", "First contribution ID is not correct");
-
-            /// Won't test the internals of the contribution as we expect the XML to be the same as in the simple case.
+            Assert.AreEqual("340656", data.ID);
+            Assert.AreEqual("7th SYMPOSIUM ON LARGE TPCs FOR LOW-ENERGY RARE EVENT DETECTION", data.Title);
         }
-#endif
-#if false
-        /// Since CERN turned on authenticaion, this particular guy is no longer accessible, so we will skip this test. When I get around to implementing
-        /// authentication, then this will be back in...
-        [TestMethod]
-        public void GetNormalSimpleMeetingAgenda()
-        {
-            /// Make sure we normalize this guy correctly.
-            string url = "http://indico.cern.ch/conferenceDisplay.py?confId=33078";
-            AgendaInfo a = new AgendaInfo(url);
-            var data = a.GetNormalizedConferenceData();
-
-            Assert.IsTrue(data.ID == 33078, "Conference ID is incorrect.");
-            Assert.IsTrue(data.Site == "indico.cern.ch", "Site is incorrect");
-            Assert.IsTrue(data.Title == "CSC Note 10: Dijet Calibration Working Group Meeting", "Conference title is incorrect.");
-            Assert.IsTrue(data.StartDate == new DateTime(2008, 04, 29, 16, 30, 0), "Meeting start date is not right");
-            Assert.IsTrue(data.EndDate == new DateTime(2008, 04, 29, 19, 25, 0), "Meeting end date is not right");
-
-            Assert.IsTrue(data.Sessions.Length == 1, "Should be only a single session in this meeting");
-            Assert.IsTrue(data.Sessions[0].ID == 0, "Null session for meeting should have ID 0");
-
-            Assert.IsTrue(data.Sessions[0].Title == data.Title, "Session title is not the same as the meeting title!");
-            Assert.IsTrue(data.Sessions[0].StartDate == data.StartDate, "Session start time is not the same as the meeting start time.");
-            Assert.IsTrue(data.Sessions[0].EndDate == data.EndDate, "Session end date is not right either");
-
-            Assert.IsTrue(data.Sessions[0].Talks.Length == 4, "Wrong number of talks in the conference");
-            Assert.IsTrue(data.Sessions[0].Talks[0].SlideURL.StartsWith("http://indico.cern.ch"), "URL is not correct");
-            Assert.IsTrue(data.Sessions[0].Talks[0].ID == 2, "ID of first talk is not right");
-            Assert.IsTrue(data.Sessions[0].Talks[0].Title == "Performance of alternative implementations of System 8", "Title of first talk is not correct");
-            Assert.IsTrue(data.Sessions[0].Talks[0].StartDate == new DateTime(2008, 4, 29, 16, 30, 0), "start time of talk is not right");
-            Assert.IsTrue(data.Sessions[0].Talks[0].EndDate == new DateTime(2008, 4, 29, 16, 45, 0), "start time of talk is not right");
-            Assert.IsTrue(data.Sessions[0].Talks[0].Speakers != null, "Speaker array should not be zero!");
-            Assert.IsTrue(data.Sessions[0].Talks[0].Speakers.Length == 1, "Speaker array should be of length 1");
-            Assert.IsTrue(data.Sessions[0].Talks[0].Speakers[0] == "Orin Harris (University of Washington)", "Speaker's name is not right!");
-
-            Assert.IsTrue(data.Sessions[0].Talks[1].ID == 3, "ID of second talk not right");
-            Assert.IsTrue(data.Sessions[0].Talks[1].SlideURL.StartsWith("http://indico.cern.ch"), "URL of second talk not right!");
-        }
-#endif
-
-#if false
-        [TestMethod]
-        public void GetNormalConferenceMeetingAgenda()
-        {
-            AgendaInfo a = new AgendaInfo(14475);
-            var data = a.GetNormalizedConferenceData();
-
-            Assert.IsTrue(data.ID == "14475", "Conference ID is incorrect.");
-            Assert.IsTrue(data.Title == "ATLAS b-tagging workshop", "Conference title is incorrect");
-            Assert.IsTrue(data.StartDate == new DateTime(2007, 05, 10, 9, 00, 00), "Start date of meeting is not right!");
-            Assert.IsTrue(data.EndDate == new DateTime(2007, 05, 11, 18, 00, 00), "Start date of meeting is not right!");
-
-            Assert.IsTrue(data.Sessions != null, "The conference should have sessions.");
-            Assert.IsTrue(data.Sessions.Length == 11, "The meeting does not have the right number of sessions!");
-
-            Assert.IsTrue(data.Sessions[0].ID == "8", "The first session ID is not 8");
-            Assert.IsTrue(data.Sessions[1].ID == "11", "The second session ID is not 11");
-
-            Assert.IsTrue(data.Sessions[0].Title == "Introduction I", "Title of first session is not correct!");
-            Assert.IsTrue(data.Sessions[0].StartDate == new DateTime(2007, 05, 10, 9, 15, 0), "Start time is not correct");
-            Assert.IsTrue(data.Sessions[0].EndDate == new DateTime(2007, 05, 10, 10, 30, 0), "Start time is not correct");
-
-            Assert.IsTrue(data.Sessions[0].Talks.Length == 5, "First session has incorrect number of contributions");
-            Assert.IsTrue(data.Sessions[1].Talks.Length == 4, "Second session has incorrect number of contributions");
-
-            Assert.IsTrue(data.Sessions[0].Talks[0].ID == "21", "First contribution ID is not correct");
-            Assert.IsTrue(data.Sessions[0].Talks[4].ID == "23", "First contribution ID is not correct");
-        }
-#endif
     }
 }
