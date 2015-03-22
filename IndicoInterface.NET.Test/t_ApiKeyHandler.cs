062fc388946a197e676e520ba863ef759e7f4cd8
Gordon Watts <gordon@gordonandpaula.net>
2015-03-22 01:25:38 +0100

    Check alpha ordering
    Everything should be lower case

--- IndicoInterface.NET.Test/t_ApiKeyHandler.cs
+++ IndicoInterface.NET.Test/t_ApiKeyHandler.cs
@@ -49,6 +49,22 @@ namespace IndicoInterface.NET.Test
         }
 
         [TestMethod]
+        public void UriWithMixedCaseParam1()
+        {
+            // Should be sorted ignoring case
+            var s = ApiKeyHandler.IndicoEncode("/export/categ/2636.ics", new Dictionary<string, string> { { "limit", "10" }, { "Fork", "dude" } }, "00000000-0000-0000-0000-000000000000", "");
+            Assert.AreEqual("/export/categ/2636.ics?apikey=00000000-0000-0000-0000-000000000000&Fork=dude&limit=10", s);
+        }
+
+        [TestMethod]
+        public void UriWithMixedCaseParam2()
+        {
+            // Should be sorted ignoring case
+            var s = ApiKeyHandler.IndicoEncode("/export/categ/2636.ics", new Dictionary<string, string> { { "Limit", "10" }, { "fork", "dude" } }, "00000000-0000-0000-0000-000000000000", "");
+            Assert.AreEqual("/export/categ/2636.ics?apikey=00000000-0000-0000-0000-000000000000&fork=dude&Limit=10", s);
+        }
+
+        [TestMethod]
         public void SimpleUriWithApiKeyAndSecret()
         {
             // Test case generated with tool found at http://indico.readthedocs.org/en/latest/http_api/tools/
@@ -64,6 +80,12 @@ namespace IndicoInterface.NET.Test
             var dt = 1426720253.FromUnixTime();
             var s = ApiKeyHandler.IndicoEncode("/export/categ/2636.ics", new Dictionary<string, string> { { "limit", "10" }, { "fork", "dude" } }, "00000000-0000-0000-0000-000000000000", "00000000-0000-0000-0000-000000000010", dt);
             Assert.AreEqual("/export/categ/2636.ics?timestamp=1426720253&apikey=00000000-0000-0000-0000-000000000000&signature=d2d4defc041097be9a29df7f7101cd356621e910", s);
+        }
+
+        [TestMethod]
+        public void EncodeMySecret()
+        {
+            // This test will only work with my secret key and api... So don't uncomment it unless you plan on stealing my stuff! :-)
         }
     }
 }
