b4486e1fe0655780d821eb035643a2e32e8520af
Gordon Watts <gordon@gordonandpaula.net>
2016-12-25 23:24:07 -0500

    Code to push the package to nuget

---
+++ IndicoInterface.NET/PushPackage.ps1
@@ -0,0 +1,20 @@
+﻿# Push the package if the version number has changed.
+#
+[CmdletBinding()]
+param()
+
+# Get the current package numbers
+$pkgName = "IndicoInterface.NET"
+$current = $(nuget list $pkgName)
+$version = $current.Split()[1]
+Write-Verbose "Nuget has version $version of Package $pkgName"
+
+# Get the current contents of the nuspec file
+$nuspec = Get-Content .\IndicoInterface.NET.nuspec | Select-String $version
+if ($nuspec.Count -gt 0) {
+	Write-Verbose "Version $version is already on the myget server. Will not push."
+} else {
+    rm .\*.nupkg
+    nuget pack -Prop Configuration=Release .\IndicoInterface.NET.csproj
+	nuget push .\IndicoInterface.NET.*.nupkg -Source https://www.nuget.org
+}
